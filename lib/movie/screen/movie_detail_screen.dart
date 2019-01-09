1bea367432a1a87522b13e5afb10220fb4a138f7
Potikorn <p.thammo@20scoops.net>
2019-01-09 15:40:02 +0700

    wrap movid detail with FutureBuilder

--- lib/movie/screen/movie_detail_screen.dart
+++ lib/movie/screen/movie_detail_screen.dart
@@ -79,22 +79,16 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(
-                          '${widget.data.title}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: Text(
-                            '${widget.data.overView}',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          ),
+                        FutureBuilder(
+                          future: fetchMovieDetail(),
+                          builder:
+                              (context, AsyncSnapshot<MovieDetails> snapshot) {
+                            if (snapshot.hasData) {
+                              return _buildTitleAndOverview(snapshot.data);
+                            } else {
+                              return Container(width: 0.0, height: 0.0);
+                            }
+                          },
                         ),
                         FutureBuilder(
                           future: fetchCrews(),
@@ -119,6 +113,53 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
     );
   }
 
+  Widget _buildTitleAndOverview(MovieDetails details) {
+    final releaseDate = details.releaseDate.split('-').first;
+    final runTimeHours = details.runtime ~/ 60;
+    final runTimeMinute = details.runtime % 60;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          '${details.title}',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24.0,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Text(
+              '$releaseDate',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14.0,
+              ),
+            ),
+            SizedBox(width: 16.0),
+            Text(
+              '$runTimeHours Hours $runTimeMinute min',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14.0,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(
+            '${details.overView}',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _buildCastsAndCrewSection(CreditsResponse creditResponse) {
     String castsText = "";
     final topCasts = creditResponse.cast.take(5).toList();
@@ -132,7 +173,7 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
           children: <Widget>[
             Text('Casts: ', style: TextStyle(color: Colors.white)),
             Expanded(
-              child: Text(castsText, style: TextStyle(color: Colors.white)),
+              child: Text(castsText, style: TextStyle(color: Colors.grey)),
             ),
           ],
         ),
@@ -141,7 +182,7 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
           children: <Widget>[
             Text('Director: ', style: TextStyle(color: Colors.white)),
             Expanded(
-              child: Text(director.name, style: TextStyle(color: Colors.white)),
+              child: Text(director.name, style: TextStyle(color: Colors.grey)),
             ),
           ],
         ),
@@ -158,6 +199,19 @@ class MovieDetailScreenState extends State<MovieDetailScreen> {
     );
     if (response.statusCode == 200) {
       return CreditsResponse.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load post ${response.statusCode}');
+    }
+  }
+
+  Future<MovieDetails> fetchMovieDetail() async {
+    var uri = Uri.https(url, '/3/movie/${widget.data.id}', queryParameters);
+    final response = await http.get(
+      uri,
+      headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return MovieDetails.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load post ${response.statusCode}');
     }
